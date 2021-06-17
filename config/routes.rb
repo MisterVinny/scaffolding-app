e26c268bffae1a333219f1eaeed855b8b21d445f
vinnyk <ragdolleye@gmail.com>
2021-06-17 10:22:10 -0500

    another test destroyed. Delete me and try again.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get 'post/index' => "posts#index"
+  get 'post/show'
+  get 'post/edit'
+  get 'post/update'
+  get 'welcome/index'
   resources :posts
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+posts = Post.create!([
+  {
+    name: "post 1",
+    title: "Just a post",
+    content: "Blah blah this is first post"
+  },
+  {
+    name: "post 2",
+    title: "Just another post",
+    content: "Blah blah this is second post"
+  },
+  {
+    name: "post 3",
+    title: "Wow, what a post",
+    content: "Way to go, another post."
+  },
+  {
+    name: "post 4",
+    title: "I can't even.",
+    content: "Blah blah. I'm just sayin' stuff."
+  },
+  {
+    name: "post 5",
+    title: "Hey you",
+    content: "Over there with the cigarette and track suit. I see you."
+  }
+])
